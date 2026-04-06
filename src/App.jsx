d00380203027f9ea70943b112efd715aa341dc5e
Mohammed Sohaib Syed <sohaibmsyed57@gmail.com>
2026-04-05 22:31:06 -0400

    moved checkout files into seperate folder and other changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { HomePage } from './pages/HomePage'
 import { Routes, Route } from 'react-router'
-import { CheckoutPage } from './pages/CheckoutPage'
+import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrderPage } from './pages/OrderPage'
 import './App.css'
 
