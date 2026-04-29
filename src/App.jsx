4db6c7ab70869e835ae92bf646cf5caa4988107b
Mohammed Sohaib Syed <sohaibmsyed57@gmail.com>
2026-04-29 18:00:10 -0400

    create add to cart feature

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Routes, Route } from 'react-router'
 import axios from 'axios';
 import { useEffect, useState} from 'react'
 
-import { HomePage } from './pages/HomePage'
+import { HomePage } from './pages/home/HomePage'
 import { CheckoutPage } from './pages/checkout/CheckoutPage'
 import { OrderPage } from './pages/OrderPage'
 import { TrackingPage } from './pages/TrackingPage'
@@ -12,17 +12,18 @@ import './App.css'
 function App() {
   const [cart, setCart] = useState([]);
 
-   useEffect(() => {
-    axios.get('/api/cart-items?expand=product')
-      .then((response) => {
-        setCart(response.data)
-      })
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }  
 
+  useEffect(() => {
+    loadCart();
   }, [])
 
   return (
     <Routes> 
-      <Route index element={<HomePage cart={cart}/>} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart}/>} />
       <Route path="order" element={<OrderPage cart={cart}/>} />
       <Route path="tracking" element={<TrackingPage cart={cart}/>} />
--- src/pages/checkout/CheckoutPage.jsx
+++ src/pages/checkout/CheckoutPage.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
-import dayjs from 'dayjs'
 import { useState, useEffect } from 'react'
 import { CheckoutHeader } from './CheckoutHeader'
+import { OrderSummary } from './OrderSummary'
 import { formatMoney } from '../../utils/money';
 import './CheckoutPage.css'
 
@@ -32,78 +32,7 @@ export function CheckoutPage({ cart }) {
         <div className="page-title">Review your order</div>
 
         <div className="checkout-grid">
-          <div className="order-summary">
-            {deliveryOptions.length > 0 && cart.map((cartItem) => {
-              const selectedDeliveryOption = deliveryOptions
-                .find((deliveryOption) => {
-                  return deliveryOption.id === cartItem.deliveryOptionId;
-                })
-
-              return (
-                <div key={cartItem.id} className="cart-item-container">
-                  <div className="delivery-date">
-                    Delivery Date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM, D')}
-                  </div>
-
-                  <div className="cart-item-details-grid">
-                    <img className="product-image"
-                      src={cartItem.product.image} />
-
-                    <div className="cart-item-details">
-                      <div className="product-name">
-                        {cartItem.product.name}
-                      </div>
-                      <div className="product-price">
-                        {formatMoney(cartItem.product.priceCents)}
-                      </div>
-                      <div className="product-quantity">
-                        <span>
-                          Quantity: <span className="quantity-label">{cartItem.quantity}</span>
-                        </span>
-                        <span className="update-quantity-link link-primary">
-                          Update
-                        </span>
-                        <span className="delete-quantity-link link-primary">
-                          Delete
-                        </span>
-                      </div>
-                    </div>
-
-                    <div className="delivery-options">
-                      <div className="delivery-options-title">
-                        Choose a delivery option:
-                      </div>
-                      {deliveryOptions.map((deliveryOption) => {
-                        let priceString = 'FREE Shipping';
-
-                        if (deliveryOption.priceCents > 0) {
-                          priceString = `${formatMoney(deliveryOption.priceCents)} - Shipping`;
-                        }
-
-                        return (
-                          <div key={deliveryOption.id} className="delivery-option">
-                            <input type="radio"
-                              checked={deliveryOption.id === cartItem.deliveryOptionId}
-                              className="delivery-option-input"
-                              name={`delivery-option-${cartItem.productId}`} />
-                            <div>
-                              <div className="delivery-option-date">
-                                {dayjs(deliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM, D')}
-                              </div>
-                              <div className="delivery-option-price">
-                                {priceString}
-                              </div>
-                            </div>
-                          </div>
-                        );
-                      })}
-
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
-          </div>
+          <OrderSummary cart={cart} deliveryOptions={deliveryOptions}/>
 
           <div className="payment-summary">
             <div className="payment-summary-title">
